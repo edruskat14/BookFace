76743c259cef07f58e4a178947f45469d093d1a4
ed <edruskat14@gmail.com>
2018-09-11 14:06:30 -0400

    Create skeleton and render first text in the browser

---
+++ frontend/components/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Popular social networking site known as BookFace</h1>
+    </div>
+  );
+};
+
+export default App;
