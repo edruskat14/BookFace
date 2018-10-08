ac8e12d2c5c4b83d18fe2d5510c5ffc85a09e7d9
ed <edruskat14@gmail.com>
2018-10-08 10:24:49 -0400

    Style comment delete button

--- frontend/components/comments/comment_index_item.jsx
+++ frontend/components/comments/comment_index_item.jsx
@@ -18,23 +18,27 @@ const mdp = (dispatch) => {
 };
 
 const CommentIndexItem = (props) => {
-  let delButton = null;
+  let delButton = <div className='comment-del-dummy'></div>;
   const commenter = props.commenter || { id : null, username: '' }
 
   if (props.currentUser.id === props.comment.commenter_id) {
     delButton =   <button onClick={() => props.deleteComment(props.comment)} className='delete-comment-button'>Remove</button>
   }
   return (
-    <div className='comment-main'>
-      <header className='comment-name'>
-        <Link to={`/users/${commenter.id}`} className='comment-name-link'>
-          {commenter.username}
-        </Link>
-      </header>
-      <div className='comment-body'>
-        {props.comment.body}
+    <div className='comment-all'>
+      <div className='comment-main'>
+        <header className='comment-name'>
+          <Link to={`/users/${commenter.id}`} className='comment-name-link'>
+            {commenter.username}
+          </Link>
+        </header>
+        <div className='comment-body'>
+          {props.comment.body}
+        </div>
       </div>
-      {delButton}
+      <div className='delete-comment-div'>
+        {delButton}
+      </div>
     </div>
   );
 };
