afe0cf1535ff767f8e2a6a365695e270e5d35ee4
ed <edruskat14@gmail.com>
2018-10-30 20:28:15 -0400

    Modify friend show

--- frontend/components/user_profile/user_show_options/friends.jsx
+++ frontend/components/user_profile/user_show_options/friends.jsx
@@ -10,7 +10,6 @@ const msp = (state) => {
 }
 
 const Friends = (props) => {
-  let ships = [];
   ships = props.friendships.map((ship) => {
     if (ship.friender_id === props.pageOwner.id) {
       return <UserDisplay user={props.users[ship.friendee_id]} key={ship.friendee_id}/>
@@ -19,9 +18,7 @@ const Friends = (props) => {
     }
   })
   return (
-    <div className='show-page-post-section'>
-      {ships}
-    </div>
+    {ships}
   )
 }
 
