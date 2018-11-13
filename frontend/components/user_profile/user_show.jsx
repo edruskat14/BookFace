91bfa4e53dd08a8c3dedaed16a2f9f1851bdbc5a
ed <edruskat14@gmail.com>
2018-11-12 19:45:07 -0500

    Attempt to fix friendship fetching

--- frontend/components/user_profile/user_show.jsx
+++ frontend/components/user_profile/user_show.jsx
@@ -13,11 +13,12 @@ class userShow extends React.Component {
     this.handleState = this.handleState.bind(this);
   }
   componentWillReceiveProps(nextProps) {
+
     if (this.props.match.params.userId !== nextProps.match.params.userId) {
       this.props.fetchAllPosts(nextProps.match.params.userId, false);
       this.props.fetchUsers();
       this.props.fetchFriends(nextProps.match.params.userId);
-      this.props.fetchFriendships(this.props.match.params.userId);
+      this.props.fetchFriendships(nextProps.match.params.userId);
     }
   }
   componentDidUpdate() {
